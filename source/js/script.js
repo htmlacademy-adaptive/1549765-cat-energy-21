776cc73ac0736e65ad7eed04f47de98bdfaf93b5
Olga <helgavernaya@gmail.com>
2021-01-19 12:05:50 +0300

    Правки-2

--- source/js/script.js
+++ source/js/script.js
@@ -10,3 +10,22 @@ navToggle.addEventListener("click", function() {
     navMain.classList.remove("main-nav--opened");
   }
 });
+
+var formToggle = document.querySelector(".form__button");
+var formInput = document.querySelectorAll(".field__input")
+
+formToggle.addEventListener("click", function(Event) {
+var error
+
+  formInput.forEach(function(item, i, formInput)  {
+    item.classList.remove("field__input--error");
+    if (item.value == "") {
+      item.classList.add("field__input--error");
+      error = true;
+    }
+  })
+
+  if (error == true) {
+        Event.preventDefault(".form__button");
+    }
+ });
