1ad2d1c53b21abb861eeec9e2c81024c796f665e
Olga <helgavernaya@gmail.com>
2021-01-20 18:02:39 +0300

    Доступ,правки

---
+++ source/js/script.js
@@ -0,0 +1,33 @@
+var navMain = document.querySelector(".main-nav");
+var navToggle = document.querySelector(".main-nav__toggle");
+
+navMain.classList.remove("main-nav--nojs");
+
+navToggle.addEventListener("click", function() {
+  if (navMain.classList.contains("main-nav--closed")) {
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
+    } else {
+    navMain.classList.add("main-nav--closed");
+    navMain.classList.remove("main-nav--opened");
+  }
+});
+
+var formToggle = document.querySelector(".form__button");
+var formInput = document.querySelectorAll(".field__input")
+
+formToggle.addEventListener("click", function(Event) {
+  var error
+
+  formInput.forEach(function(item, i, formInput)  {
+    item.classList.remove("field__input--error");
+    if (item.value == "") {
+      item.classList.add("field__input--error");
+      error = true;
+    }
+  })
+
+  if (error == true) {
+    Event.preventDefault(".form__button");
+  }
+});
